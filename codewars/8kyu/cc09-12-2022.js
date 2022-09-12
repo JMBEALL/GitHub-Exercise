c1bc7a42b2d16c80ea2a6e27c7604ad34f54597f
Jordan Beall <JordanBeall@me.com>
2022-09-12 14:41:23 -0500

    Implementing solution to opposite number

---
+++ codewars/8kyu/cc09-12-2022.js
@@ -0,0 +1,18 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+function opposite(number) {
+    //your code here
+    if (number > 0) {
+      return -1 * number
+    } else {
+      return number * -1
+    }
+  }
+  
