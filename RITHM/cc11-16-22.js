5c5165ed0f978f929e16970c59bb4aec88340cf7
Jordan Beall <JordanBeall@me.com>
2022-11-16 13:24:40 -0600

    Implementing solution for appentToSTRING

---
+++ RITHM/cc11-16-22.js
@@ -0,0 +1,15 @@
+// appendToString
+
+// The function should return a new string which consists of the second string appended to the first string.
+
+// Examples:
+
+// appendToString("Hello", " World!"); // "Hello World!"
+// appendToString("Foo", "bar"); // "Foobar"
+// appendToString("bar", "Foo"); // "barFoo"
+// appendToString("", "test"); // "test"
+// appendToString("other test", ""); // "other test"
+
+function appendToString(str1, str2) {
+    return str1 + str2;
+  }
