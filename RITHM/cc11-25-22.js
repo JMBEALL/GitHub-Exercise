3002c9428761d8e2b6150ebbf51ed7ad19fc08e5
Jordan Beall <JordanBeall@me.com>
2022-11-25 13:42:43 -0600

    Implementing solution for keys

---
+++ RITHM/cc11-25-22.js
@@ -0,0 +1,25 @@
+// keys
+
+// Write a function called keys, which accepts an object and returns an array of all of the keys in the object.
+
+// Do not use the built in Object.keys() function!
+
+// Examples:
+
+// let obj = { a: 1, b: 2, c: 3 };
+// keys(obj); // ["a", "b", "c"]
+
+// let obj2 = { first: 'Matt', last: 'Lane' };
+// keys(obj2); // ["first", "last"]
+
+// let obj3 = {};
+// keys(obj3); // []
+
+function keys(obj) {
+    let arr = [];
+    for (let key in obj) {
+      arr.push(key);
+    }
+    return arr;
+  }
+  
