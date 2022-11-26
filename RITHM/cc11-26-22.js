ff47c0528aaf753a0bed8f6fdb79c4b3fb99b6eb
Jordan Beall <JordanBeall@me.com>
2022-11-26 13:44:02 -0600

    Implementing solution for lastIndexOf

---
+++ RITHM/cc11-26-22.js
@@ -0,0 +1,23 @@
+// lastIndexOf
+
+// Write a function called lastIndexOf, which accepts an array and a number.
+
+// The function should return the last index at which the value exists, or -1 if the value is not found.
+
+// Do not use the built in Array.lastIndexOf() function!
+
+// Examples:
+
+// lastIndexOf([1, 2, 3, 4], 2); // 1
+// lastIndexOf([1, 2, 3, 4, 2], 2); // 4
+// lastIndexOf([1, 2, 3, 4], 22); // -1
+
+function lastIndexOf(arr, val) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+      if (arr[i] === val) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  
