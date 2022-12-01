7686143d86e6fce565569e089cbb6c65e814a5a3
Jordan Beall <JordanBeall@me.com>
2022-12-01 13:55:59 -0600

    Implementing solution for pluck

---
+++ RITHM/cc12-1-22.js
@@ -0,0 +1,27 @@
+// pluck
+
+// Write a function called pluck, which takes an array of objects and the name of a key.
+
+// The function should return an array containing the value associated with that key for each object, or undefined if that key is not present in the object.
+
+// Examples:
+
+// pluck([
+//   { name: "Tim" }, { name: "Matt" }, { name: "Elie" }],
+//  'name'
+// )
+// // ["Tim", "Matt", "Elie"]
+
+// pluck(
+//   [{ name: "Tim", isBoatOwner: true }, { name: "Matt", isBoatOwner: false }, { name: "Elie" }],
+//  'isBoatOwner'
+// )
+// // [true, false, undefined]
+
+function pluck(arr, key) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      newArr.push(arr[i][key]);
+    }
+    return newArr;
+  }
